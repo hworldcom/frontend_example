3524274af567b7f44b661f29cad20d8a89409710
Hoang L <hoang@Hoangs-MacBook-Air.local>
2024-09-01 15:47:48 +0200

    add more styling

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Note.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel;
@@ -10,12 +11,21 @@ interface NoteProps {
 const Note = ({ note, className }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
 
+  let createdUpdatedText: string;
+
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  } // TODO: replace with useEffect
+
   return (
     <Card className={`${styles.noteCard} ${className}`}>
-      <Card.Body>
+      <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
+      <Card.Footer>{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
